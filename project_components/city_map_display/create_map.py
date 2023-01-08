04b4dfe47fea8d3ad3ec0801e2840543506d3b64
junnjiee16 <junnjiee16@gmail.com>
2023-01-08 16:43:35 +0800

    map making function

--- project_components/city_map_display/create_map.py
+++ project_components/city_map_display/create_map.py
@@ -1,38 +1,63 @@
 import turtle
 import os
 
-# create wall class
-class Block(turtle.Turtle):
+# create tiles of different colors across the map
+class Tile(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("square")
-        self.color('grey')
-        self.penup()
-        self.speed(0)
+        self.shapesize(24/20) # set size to be equal to the pixels we move
+        self.pencolor('black') # color of the square border
+        self.penup() # no drawing when moving 
+        self.speed(0) # fastest speed
+
+
+# function to create map
+def create_map(map_layout):
+    window = turtle.Screen()
+    window.title("PIZZA RUNNERS: Done by Lim Jun Jie, Timothy Chia, DAAA/FT/2B/02") # create the titlebar
+    window.setup(700,700)
+
+    for y_pos in range(len(map_layout)):
+        for x_pos in range(len(map_layout[y_pos])):
+            # calculate position of grid
+            grid_x_pos = -288 + (24 * x_pos)
+            grid_y_pos = 288 - (24 * y_pos)
+
+            # check if it is a wall
+            if map_layout[y_pos][x_pos] == "X":
+                tile.fillcolor('grey')
+            
+            # check if it is start/end point
+            elif map_layout[y_pos][x_pos] == "s":
+                tile.fillcolor('green')
+            elif map_layout[y_pos][x_pos] == "e":
+                tile.fillcolor('blue')
+
+            # if not anything else, it is normal road
+            else:
+                tile.fillcolor('white') 
+
+            tile.goto(grid_x_pos, grid_y_pos) # go to position
+            tile.stamp() # print grid at location
+
+    # exit application when any part of turtle screen display is clicked (except for top bar)
+    window.exitonclick()
+
+# create map
+tile = Tile()
 
 # open and read map file
-script_dir = os.path.dirname(os.path.abspath(__file__)) # get absolute path of current device (relative to this script)
-filename = ".\\test_files\\map1.txt"
+script_dir = os.path.dirname(os.path.abspath(__file__)) # get absolute path of current device leading to this script, then get directory
+filename = ".\\test_files\\map1.txt" # specify test file folder and file
 abs_file_path = os.path.join(script_dir, filename)
 
 with open(abs_file_path) as f:
-    content = f.readlines()
-content = [x.strip() for x in content] 
-
-print(script_dir)
-
-# create map
-pen = Block()
-
-window = turtle.Screen()
-window.title("PIZZA RUNNERS") # create the titlebar
-window.setup(700,700)
-for arr in content:
-    for block in arr:
-        if block == "X":
-            pen.stamp()
+    map_layout = f.readlines()
+map_layout = [x.strip() for x in map_layout] 
 
 
+# create map using function
+create_map(map_layout)
+        
 
-# exit application when any part of turtle screen display is clicked (except for top bar)
-window.exitonclick()
